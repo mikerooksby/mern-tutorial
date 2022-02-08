b9a49d151102ff38c89ec5d25ef67d0790996ce6
Mike Rooksby <mikerooksby@gmail.com>
2022-02-08 16:09:25 -0500

    Add REST functionality for Goals

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const colors = require('colors');
-const {errorHandler} = require('./middleware/errorMiddleware');
+const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 5000;
@@ -11,10 +11,15 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/goals', require('./routes/goalRoutes'));
 
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.brightWhite
+      .bold.underline
+  )
+);
